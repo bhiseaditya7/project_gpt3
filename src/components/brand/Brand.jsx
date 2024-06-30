00bee3692c8b8ce4ecf8dc6d1ab5e57f182332d7
bhiseaditya7 <bhiseaditya7@gmail.com>
2024-06-30 22:42:45 +0530

    second commit

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -6,19 +6,19 @@ const Brand = () => {
   return (
     <div className='gpt3__brand section__padding'>
       <div>
-        <a href="https://www.google.com" target = "_blank" ><img src={google} alt='google'/></a>
+        <a href="https://www.google.com" target = "_blank" rel="noreferrer" ><img src={google} alt='google'/></a>
       </div>
       <div>
-        <a href="https://slack.com" target="_blank"><img src={slack} alt='slack'/></a>
+        <a href="https://slack.com" target="_blank" rel="noreferrer"><img src={slack} alt='slack'/></a>
       </div>
       <div>
-        <a href="https://www.atlassian.com/" target="_blank"><img src={atlassion} alt='atlassion'/></a>
+        <a href="https://www.atlassian.com/" target="_blank" rel="noreferrer"><img src={atlassion} alt='atlassion'/></a>
       </div>
       <div>
-        <a href='https://www.dropbox.com/home' target='_blank'><img src={dropbox} alt='dropbox'/></a>
+        <a href='https://www.dropbox.com/home' target='_blank' rel="noreferrer"><img src={dropbox} alt='dropbox'/></a>
       </div>
       <div>
-        <a href='https://www.shopify.com/' target='_blank'><img src={shopify} alt='shopify'/></a>
+        <a href='https://www.shopify.com/' target='_blank' rel="noreferrer"><img src={shopify} alt='shopify'/></a>
       </div>
     </div>
   )
